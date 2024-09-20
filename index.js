b947311c3a371631fa07f189bd585397fa8fd583
ducksandgoats <mrtraderjoseph@gmail.com>
2024-09-19 22:46:05 -0500

    make changes

--- index.js
+++ index.js
@@ -54,14 +54,14 @@ export default class Client extends Events {
         //     delete this.socket
         // }
         this.socket = new WebSocket(`${this.url}?hash=${this.hash}&id=${this.id}&want=${6 - this.channels.size}`)
-        this.socket.addEventListener('open', handleOpen)
-        this.socket.addEventListener('message', handleMessage)
-        this.socket.addEventListener('error', handleError)
-        this.socket.addEventListener('close', handleClose)
-        const handleOpen = (e) => {
+        this.socket.addEventListener('open', this.socket.handleOpen)
+        this.socket.addEventListener('message', this.socket.handleMessage)
+        this.socket.addEventListener('error', this.socket.handleError)
+        this.socket.addEventListener('close', this.socket.handleClose)
+        this.socket.handleOpen = (e) => {
             this.emit('ev', 'socket opened' + e)
         }
-        const handleMessage = (e) => {
+        this.socket.handleMessage = (e) => {
             let message
             try {
                 message = JSON.parse(e.data)
@@ -138,22 +138,22 @@ export default class Client extends Events {
                 }
             }
         }
-        const handleError = (e) => {
+        this.socket.handleError = (e) => {
             this.emit('error', e)
         }
-        const handleClose = (e) => {
+        this.socket.handleClose = (e) => {
             this.emit('ev', 'socket closed' + e)
-            handleEvent()
+            this.socket.handleEvent()
             if(this.socket.relay){
                 setTimeout(() => {this.ws()}, 5000)
             }
             delete this.socket
         }
-        const handleEvent = () => {
-            this.socket.removeEventListener('open', handleOpen)
-            this.socket.removeEventListener('message', handleMessage)
-            this.socket.removeEventListener('error', handleError)
-            this.socket.removeEventListener('close', handleClose)
+        this.socket.handleEvent = () => {
+            this.socket.removeEventListener('open', this.socket.handleOpen)
+            this.socket.removeEventListener('message', this.socket.handleMessage)
+            this.socket.removeEventListener('error', this.socket.handleError)
+            this.socket.removeEventListener('close', this.socket.handleClose)
         }
     }
     handleChannel(channel){
